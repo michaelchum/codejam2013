65686a3adb026107fa420fcacc5f8a192f65ac84
michaelchum <michaelhochum@gmail.com>
2013-11-17 04:13:53 -0500

    GOLD

--- sgd.py
+++ sgd.py
@@ -1,12 +1,22 @@
 from sklearn.linear_model import SGDRegressor
 from numpy import genfromtxt, savetxt
+from numpy import shape
 import numpy
 import dateutil.parser
+import math
+import scipy as sp
+#from sklearn import cross_validation
+#from sklearn import datasets
+#from sklearn import metrics
+#from sklearn import linear_model
+#from sklearn import svm
+from sklearn import tree
+from sklearn.externals import joblib
 
 def main():
     #create the training & test sets, skipping the header row with [1:]
-    dataset = genfromtxt(open('datasetinter.csv','r'), dtype=None, delimiter=',', usecols=(0,1,2,3,4,5,6,7)) 
-    newdataset = numpy.zeros(dataset.shape[0]*7).reshape(dataset.shape[0],7)
+    dataset = genfromtxt(open('intro_datafo.csv','r'), dtype=None, delimiter=',')[1:] 
+    newdataset = numpy.zeros(dataset.shape[0]*8).reshape(dataset.shape[0],8)
     index = 0
     for date in dataset:
     	dateutil.parser.parse(date[0])
@@ -14,14 +24,24 @@ def main():
     	month = date[0][5:7]
     	day = date[0][8:10]
     	time = date[0][12:13] + date[0][14:16]
-    	newdata = [month,day,time,date[1],0,date[3],0,date[5]]
+    	float(month)
+    	float(day)
+    	float(time)
+    	float(date[1])
+    	float(date[3])
+    	float(date[5])
+    	if math.isnan(date[5]):
+    		date[5]=18000
+
+    	newdata = [month,day,time,0,date[2],date[3],date[4],date[5]]
     	newdataset[index] = newdata
     	index += 1
+
+    savetxt('newdataset.csv', newdataset, delimiter=',', fmt='%f')
     print newdataset
 
     testset = genfromtxt(open('sampleinput.csv','r'), dtype=None, delimiter=',',usecols = (0,1,2,3,4))
-    print testset
-    newtestset = numpy.zeros(testset.shape[0]*(5+2)).reshape(testset.shape[0],(5+2))
+    newtestset = numpy.zeros(testset.shape[0]*7).reshape(testset.shape[0],7)
     index1 = 0
 
     for date in testset:
@@ -29,21 +49,44 @@ def main():
     	month = date[0][5:7]
     	day = date[0][8:10]
     	time = date[0][12:13] + date[0][14:16]
-    	newdata = [month,day,time,date[1],0,date[3],0]
-    	newtestset[index1] = newdata
+    	float(month)
+    	float(day)
+    	float(time)
+    	float(date[1])
+    	float(date[3])
+    	newdata1 = [month,day,time,0,date[2],date[3],date[4]]
+    	newtestset[index1] = newdata1
     	index1 += 1
 
-    print newtestset
+	savetxt('newtestset.csv', newtestset, delimiter=',', fmt='%f')
 
-    target = [x[7] for x in newdataset]
-    train = [x[0:7] for x in newdataset]
-    
-    clf = SGDRegressor(loss="huber")
-    clf.fit(train, target)
+	target = [x[7] for x in newdataset]
+	train = [x[0:7] for x in newdataset]
 
-    #predicted_probs = [[x[4]] for index, x in enumerate(clf.predict(test))]
-    predicted_probs = clf.predict(newtestset)
-    savetxt('output.csv', predicted_probs, delimiter=',', fmt='%f')
+
+	#clf = SGDRegressor(loss="epsilon_insensitive")
+	#clf = linear_model.Lasso(alpha = 0.1)
+	#clf = linear_model.BayesianRidge()
+	#clf = linear_model.LassoLars(alpha=.01
+	#clf = linear_model.Ridge (alpha = 0.5)
+	#clf = svm.LinearSVC()
+	clf = tree.DecisionTreeRegressor()
+	#clf = linear_model.RidgeCV(alphas=[0.1, 1.0, 10])
+	#clf = linear_model.LinearRegression()
+
+	clf.fit(train, target)
+	#clf.coef_ 
+	#print clf.coef_
+
+	#clf = svm.SVR()
+	#clf.fit(train, target)
+
+	#predicted_probs = [[x[4]] for index, x in enumerate(clf.predict(test))]
+	predicted_probs = clf.predict(newtestset)
+	savetxt('output.csv', predicted_probs, delimiter=',', fmt='%f')
+
+	# save the classifier
+	joblib.dump(clf, 'gold.pkl')
 
 if __name__=="__main__":
     main()
